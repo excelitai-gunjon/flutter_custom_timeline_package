ac9bb7d76df5c689614518852041b2827e63eb51
nazmus <nazmus.s.ashrafi@gmail.com>
2022-12-10 10:57:44 +0600

    added- responsive app dimension file for Calculating aspect ratio based on width of our Figma design

---
+++ lib/Utils/GlobalFunctions.dart
@@ -0,0 +1,10 @@
+
+// added- responsive app dimension file for Calculating aspect ratio based on width of our Figma design
+double globalSize(double size, double width){
+// size is the size of the Widget in Figma file
+// width is the screen width of the device
+  const int figBaseWidth = 360;
+
+  double ratio = size/figBaseWidth;
+  return ratio * width;
+}
